f3ac563dab15eaeb46ab506605d34b4ea0cfd21b
lichiehwang <131224887+lichiehwang@users.noreply.github.com>
2023-07-12 11:10:15 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,6 +26,9 @@ $(document).ready(function() {
       var Box = $(element).children('.box');
       $(Box).slideDown(500);
     });
+  
+    // Show the circle1 and word elements
+    $('.circle1, .word').show();
   });
   
   // Web click event
@@ -37,38 +40,46 @@ $(document).ready(function() {
   });
 });
 
-
-
-//任意のタブにURLからリンクするための設定
-function GethashID (hashIDName){
-	if(hashIDName){
-		//タブ設定
-		$('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
-			var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得	
-			if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
-				var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
-				$('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
-				$(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
-				//表示させるエリア設定
-				$(".area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
-				$(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加	
-			}
-		});
-	}
+// Function to handle hash ID for tabs
+function GethashID(hashIDName) {
+  if (hashIDName) {
+    // Tab settings
+    $('.tab li').find('a').each(function() {
+      var idName = $(this).attr('href');
+      if (idName == hashIDName) {
+        var parentElm = $(this).parent();
+        $('.tab li').removeClass("active");
+        $(parentElm).addClass("active");
+        // Display area settings
+        $(".area").removeClass("is-active");
+        $(hashIDName).addClass("is-active");
+      }
+    });
+  }
 }
 
-//タブをクリックしたら
+// Handle tab clicks
 $('.tab a').on('click', function() {
-	var idName = $(this).attr('href'); //タブ内のリンク名を取得	
-	GethashID (idName);//設定したタブの読み込みと
-	return false;//aタグを無効にする
+  var idName = $(this).attr('href');
+  GethashID(idName);
+  return false;
 });
 
-
-// 上記の動きをページが読み込まれたらすぐに動かす
-$(window).on('load', function () {
-    $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
-    $('.area:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
-	var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
-	GethashID (hashName);//設定したタブの読み込み
+// Execute tab handling on page load
+$(window).on('load', function() {
+  $('.tab li:first-of-type').addClass("active");
+  $('.area:first-of-type').addClass("is-active");
+  var hashName = location.hash;
+  GethashID(hashName);
 });
+
+$(document).ready(function() {
+  // Handle click on image-container
+  $('.graflex .image-container').on('click', function() {
+    // Get the overlay element
+    var overlay = $(this).find('.overlay');
+    // Toggle the overlay visibility
+    overlay.toggleClass('show-overlay');
+  });
+});
+
